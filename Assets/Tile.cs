2b4f011dfcf4d94d4e1e7445301032771723cd03
= <=>
2018-04-13 23:39:21 +0200

    Made early tile generator, no object pooling

---
+++ Assets/Tile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public Vector3 endPos; //Where the tile ends for the next one to start
+    public List<Tile> possibleTiles; //Tiles that can come afte this
+}
---
+++ Assets/TileGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour {
+
+
+    public float generationDistance; //Distance the generator will generate to
+    public float deleteDistance; //Distance when the generator will start deleting pieces
+    public Tile startile;
+    
+    private List<Tile> instantiatedPieces = new List<Tile>(); //Ordered list of tiles
+
+
+    // Use this for initialization
+    void Start () {
+        instantiatedPieces.Add(Instantiate<Tile>(startile));
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //Check delete distance on first instantiated piece
+        if (Vector3.Distance(transform.position, instantiatedPieces[0].transform.position) > deleteDistance)
+        {
+            Destroy(instantiatedPieces[0].gameObject);
+            instantiatedPieces.RemoveAt(0);
+        }
+
+        //Check generation distance on the latest piece
+        if (Vector3.Distance(transform.position, instantiatedPieces[instantiatedPieces.Count - 1].transform.position) < generationDistance)
+        {
+            Tile currentTile = instantiatedPieces[instantiatedPieces.Count - 1];
+            instantiatedPieces.Add(Instantiate<Tile>(pickRandomPossibleTile(currentTile), currentTile.transform.position + currentTile.endPos, currentTile.transform.rotation));
+        }
+    }
+
+
+    private Tile pickRandomPossibleTile(Tile currentTile)
+    {
+        int index = Random.Range(0, currentTile.possibleTiles.Count);
+        return currentTile.possibleTiles[index];
+    }
+}
