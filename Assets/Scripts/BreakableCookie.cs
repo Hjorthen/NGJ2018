1f8449e0ea93dff6514a2a0914cd6f43817a7887
alexandru <alexandru@unity3d.com>
2018-04-14 15:48:08 +0200

    hooked in basic breakable to controller

--- Assets/Scripts/BreakableCookie.cs
+++ Assets/Scripts/BreakableCookie.cs
@@ -16,25 +16,45 @@ public class BreakableCookie {
     private static readonly float kMinHPPercentage = 0.0f;
 
     [SerializeField]
-    private GameObject pristineCookie;
+    private GameObject m_PristineCookie;
     [SerializeField]
-    private GameObject crackableCookie;
+    private GameObject m_CrackableCookie;
 
+    public GameObject CurrentDisplayCookie { get; private set; }
+
+    public GameObject PristineCookie
+    {
+        get { return m_PristineCookie; }
+    }
+
+    public GameObject CrackableCookie
+    {
+        get { return m_CrackableCookie; }
+    }
     public BreakableCookie()
     {
-
+        Reset();
     }
 
     public void SetCookies(GameObject pristine, GameObject crackable)
     {
-        pristineCookie = pristine;
-        crackableCookie = crackable;
+        m_PristineCookie = pristine;
+        CurrentDisplayCookie = m_PristineCookie;
+        m_CrackableCookie = crackable;
     }
 
 
     public void Reset()
     {
         m_HPPercentage = kMaxHPPercentage;
+
+        if (m_CrackableCookie != null && m_PristineCookie != null)
+        {
+            m_CrackableCookie.SetActive(false);
+            m_PristineCookie.SetActive(true);
+            CurrentDisplayCookie = m_PristineCookie;
+        }
+
     }
 
     /// <summary>
@@ -43,7 +63,15 @@ public class BreakableCookie {
     public void TakeDamage()
     {
         if (m_HPPercentage > kMinHPPercentage)
+        {
             m_HPPercentage -= 0.15f;
+            if (!m_CrackableCookie.activeSelf)
+            {
+                m_CrackableCookie.SetActive(true);
+                m_PristineCookie.SetActive(false);
+                CurrentDisplayCookie = m_CrackableCookie;
+            }
+        }
 
         if (m_HPPercentage < kMinHPPercentage)
             m_HPPercentage = kMinHPPercentage;
