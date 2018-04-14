a9d8efb06fca1d9352854e5a538b7c909feedc39
Sebastian Hjorth <sebastianh@unity3d.com>
2018-04-14 13:19:00 +0200

    Basic steering

---
+++ Assets/CookieController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieController : MonoBehaviour {
+    public WheelCollider[] LeftWheels;
+    public WheelCollider[] RightWheels;
+    public float maxSteeringAngle; 
+    void FixedUpdate () {
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        for (int i = 0; i < LeftWheels.Length; i++)
+        {
+            LeftWheels[i].steerAngle = steering;
+        }
+
+        for (int i = 0; i < RightWheels.Length; i++)
+        {
+            RightWheels[i].steerAngle = steering;
+        }
+    }
+}
