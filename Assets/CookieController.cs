5cd94cc65d6598ee7dff1a5dade39a69f2800f54
Sebastian Hjorth <sebastianh@unity3d.com>
2018-04-14 14:41:29 +0200

    Added initial velocity for CookieController

--- Assets/CookieController.cs
+++ Assets/CookieController.cs
@@ -8,18 +8,28 @@ public class CookieController : MonoBehaviour {
     public WheelCollider[] RightWheels;
     public GameObject Renderer;
     public float maxSteeringAngle;
+    public float InitialVelocity;
 
     private Rigidbody m_RigidBody;
 
     private void Start()
     {
         m_RigidBody = GetComponent<Rigidbody>();
+        for (int i = 0; i < LeftWheels.Length; i++)
+        {
+            LeftWheels[i].motorTorque = InitialVelocity;
+        }
+
+        for (int i = 0; i < RightWheels.Length; i++)
+        {
+            RightWheels[i].motorTorque = InitialVelocity;
+        }
     }
 
     private void Update()
     {
         Vector3 localVelocity = transform.InverseTransformDirection(m_RigidBody.velocity);
-        Renderer.transform.Rotate(Vector3.up, m_RigidBody.velocity.x);
+        Renderer.transform.Rotate(Vector3.up, m_RigidBody.velocity.z);
     }
 
 
