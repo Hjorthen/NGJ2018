124ac8cc75fa627f30e6630de1d3c72d388467a0
alexandru <alexandru@unity3d.com>
2018-04-14 16:15:58 +0200

    Clean up controller moved physics logic to PhysicsCookie class

--- Assets/Scripts/CookieController.cs
+++ Assets/Scripts/CookieController.cs
@@ -4,49 +4,25 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class CookieController : MonoBehaviour {
-    public WheelCollider[] LeftWheels;
-    public WheelCollider[] RightWheels;
-
-    public float maxSteeringAngle;
-    public float InitialVelocity;
 
     [SerializeField]
     private BreakableCookie breakableCookie;
-    private Rigidbody m_RigidBody;
+    [SerializeField]
+    private PhysicsCookie physicsCookie;
 
     private void Start()
     {
         breakableCookie.Reset();
-        m_RigidBody = GetComponent<Rigidbody>();
-        for (int i = 0; i < LeftWheels.Length; i++)
-        {
-            LeftWheels[i].motorTorque = InitialVelocity;
-        }
-
-        for (int i = 0; i < RightWheels.Length; i++)
-        {
-            RightWheels[i].motorTorque = InitialVelocity;
-        }
     }
 
     private void Update()
     {
-        Vector3 localVelocity = transform.InverseTransformDirection(m_RigidBody.velocity);
-        breakableCookie.CurrentDisplayCookie.transform.Rotate(Vector3.up, m_RigidBody.velocity.z);
+        breakableCookie.CurrentDisplayCookie.transform.Rotate(Vector3.up, physicsCookie.Velocity.z);
     }
 
 
     void FixedUpdate() {
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
-        for (int i = 0; i < LeftWheels.Length; i++)
-        {
-            LeftWheels[i].steerAngle = steering;
-        }
-
-        for (int i = 0; i < RightWheels.Length; i++)
-        {
-            RightWheels[i].steerAngle = steering;
-        }        
+        physicsCookie.Step(Input.GetAxis("Horizontal"));     
     }
 }
