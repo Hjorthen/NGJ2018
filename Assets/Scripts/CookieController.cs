91e0c46fdfe0a1bc3ceedc4efc995f79d241afad
alexandru <alexandru@unity3d.com>
2018-04-14 16:26:35 +0200

    updated cookie prefab cookie can now take damage wooo, and die

--- Assets/Scripts/CookieController.cs
+++ Assets/Scripts/CookieController.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class CookieController : MonoBehaviour {
 
     [SerializeField]
+    private bool DebugMode = false;
+    [SerializeField]
     private BreakableCookie breakableCookie;
     [SerializeField]
     private PhysicsCookie physicsCookie;
-
+    public float collisionMagnitudeThreshold;
     private void Start()
     {
         breakableCookie.Reset();
@@ -23,6 +25,19 @@ public class CookieController : MonoBehaviour {
 
     void FixedUpdate() {
 
-        physicsCookie.Step(Input.GetAxis("Horizontal"));     
+        if (!breakableCookie.isDead())
+            physicsCookie.Step(Input.GetAxis("Horizontal"));
+        else
+            physicsCookie.Shutdown();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (physicsCookie.Velocity.sqrMagnitude >= collisionMagnitudeThreshold)
+        {
+            breakableCookie.TakeDamage();
+            if (DebugMode)
+                Debug.Log("Cookie took damage", gameObject);
+        }
     }
 }
