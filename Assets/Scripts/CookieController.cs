d10fa29385f948725cb48209186bbb69ccfc0cda
Rares-Sebastian Popa <Rares_Popa_2008@Yahoo.com>
2018-04-14 17:24:11 +0200

    * Cookie uses distance travelled vs velocity to compute rotation

--- Assets/Scripts/CookieController.cs
+++ Assets/Scripts/CookieController.cs
@@ -3,45 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class CookieController : MonoBehaviour {
+public class CookieController : MonoBehaviour
+{
+    [SerializeField]
+    private BreakableCookie m_breakableCookie;
+    [SerializeField]
+    private PhysicsCookie m_physicsCookie;
+    [SerializeField]
+    private float m_collisionMagnitudeThreshold;
+    [SerializeField]
+    private float m_distanceTravelledMultiplier = 1;
 
     [SerializeField]
-    private bool DebugMode = false;
-    [SerializeField]
-    private BreakableCookie breakableCookie;
-    [SerializeField]
-    private PhysicsCookie physicsCookie;
-    public float collisionMagnitudeThreshold;
-    private SimpleTimer timer;
-    private void Start()
+    private bool m_debugMode = false;
+    private SimpleTimer m_timer;
+    private Vector3 m_previousPosition;
+
+    private void Awake()
     {
-        timer = new SimpleTimer();
-        breakableCookie.Reset();
+        m_previousPosition = transform.position;
+        m_timer = new SimpleTimer();
+        m_breakableCookie.Reset();
     }
 
     private void Update()
     {
-        breakableCookie.CurrentDisplayCookie.transform.Rotate(Vector3.up, physicsCookie.Velocity.z);
+        float distanceTravelled = Vector3.Distance(m_previousPosition, transform.position);
+        m_breakableCookie.CurrentDisplayCookie.transform.localEulerAngles += new Vector3(360 * distanceTravelled * m_distanceTravelledMultiplier, 0, 0);
     }
 
+    void FixedUpdate()
+    {
 
-    void FixedUpdate() {
-
-        if (!breakableCookie.isDead())
-            physicsCookie.Step(Input.GetAxis("Horizontal"));
+        if (!m_breakableCookie.isDead())
+            m_physicsCookie.Step(Input.GetAxis("Horizontal"));
         else
-            physicsCookie.Shutdown();
+            m_physicsCookie.Shutdown();
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (physicsCookie.Velocity.sqrMagnitude >= collisionMagnitudeThreshold && timer.IsDone())
+        if (m_physicsCookie.Velocity.sqrMagnitude >= m_collisionMagnitudeThreshold && m_timer.IsDone())
         {
-            breakableCookie.TakeDamage();
-            timer.Start(1);
-            if (DebugMode)
+            m_breakableCookie.TakeDamage();
+            m_timer.Start(1);
+            if (m_debugMode)
                 Debug.Log("Cookie took damage", gameObject);
-            
+
         }
     }
 }
