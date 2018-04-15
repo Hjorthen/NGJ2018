fe6d1952fbc540d9a3b7dd5587a6f82cdc67ec2b
Sebastian Hjorth <sebastianh@unity3d.com>
2018-04-15 12:49:16 +0200

    Fixed merge error

--- Assets/Scripts/RespawnInterface.cs
+++ Assets/Scripts/RespawnInterface.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Assets/RespawnInterface.cs
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,21 +18,3 @@ public class RespawnInterface : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RespawnInterface : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
->>>>>>> 2489432ef4baa295ecd08c82a07ae7182e10d296:Assets/Scripts/RespawnInterface.cs
